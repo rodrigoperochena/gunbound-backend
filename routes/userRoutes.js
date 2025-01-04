6bb961855d94e67dd7d9246ec8d8cd40c0ade344
tak <taksumaq@ubuntu.server>
2025-01-05 04:22:57 +0800

    Updated userRoutes with new and refactored API endpoints
    - Replaced  route with  for dynamic player activity queries.
    - Added new  route to fetch all users sorted by score.
    - Introduced  route to fetch individual player details.
    - Removed outdated  route to simplify routing structure.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,20 @@
 // routes/userRoutes.js
 const express = require('express');
-const { registerUser, checkLastSeenUsers, getUserDetails } = require('../controllers/userController');
+const { registerUser, getUsers, getUserById, lastSeenUsers } = require('../controllers/userController');
 
 const router = express.Router();
 
 // Register route
 router.post('/register', registerUser);
 
-// Check users login route
-router.get('/last-seen-users', checkLastSeenUsers);
+// Fetch all users
+router.get('/', getUsers);
 
-// Get user details route
-router.get('/details/:id', getUserDetails)
+// Check users login route
+router.get('/last-seen', lastSeenUsers);
+
+// Fetch a single user by ID
+router.get('/:id', getUserById)
+
 
 module.exports = router;
